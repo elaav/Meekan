5f9aa6b1de43219bb75d098aa4a2d2fca2b87e5e
ela <ela@il.ibm.com>
2016-06-30 22:31:45 +0300

    testing the average bot

--- average_bot.py
+++ average_bot.py
@@ -19,13 +19,14 @@ def is_float(str):
         float(str)
         # Yeah, i'm sure there's a better way to do this.
         #  might be some regex like r"^[-+]?(?:\b[0-9]+(?:\.[0-9]*)?|\.[0-9]+\b)(?:[eE][-+]?[0-9]+\b)?$"
-        if str in ["NaN", "-NaN", "+NaN", "infinity", "-iNF", "nan", "-nan", "+nan"]:
+        if str in ['NaN', '-NaN', '+NaN', 'infinity', '-iNF', 'nan', '-nan', '+nan']:
             return False
         return True
     except ValueError:
         return False
 
 # Calculating the average of all users
+# @param sums_dict: dictionary of dictionaries {user:{sum:[float], count:[int]}}
 def calc_all_average(sums_dict):
     all_sum = sum([x.get("sum") for x in sums_dict.values()])
     counter = sum([x.get("count") for x in sums_dict.values()])
@@ -37,13 +38,13 @@ def calc_all_average(sums_dict):
 # Updating the sums dict with the new number
 # @param sums: sums dictionary of the form {user:{sum:int , count:int}}
 # @param user: the user to update in the sums dict
-# @param message: the str message with the new number
-def update_sums(sums, user, message):
+# @param number: the number to add to the sum
+def update_sums(sums, user, number):
     if not sums.has_key(user):
         sums[user] = {}
         sums[user][u'sum'] = 0
         sums[user][u'count'] = 0
-    sums[user][u'sum'] += float(message)
+    sums[user][u'sum'] += number
     sums[user][u'count'] += 1
 
 
@@ -66,22 +67,22 @@ def main():
         while True:
             # Read latest messages
             for slack_message in sc.rtm_read():
-                message = slack_message.get("text")
-                user = slack_message.get("user")
-                channel = slack_message.get("channel")
+                message = slack_message.get('text')
+                user = slack_message.get('user')
+                channel = slack_message.get('channel')
                 if not message or not user:
                     continue
                 # Assuming the only messages to get numbers from are with numbers only (no text)
                 if is_float(message):
-                    update_sums(sums, user, message)
-                    average = sums[user].get("sum") / sums[user].get("count")
+                    update_sums(sums, user, float(message))
+                    average = sums[user].get('sum') / sums[user].get('count')
                     # Assuming it should write the user's average in the latest channel it got the message from
-                    sc.rtm_send_message(channel, "<@{}> Average: {}".format(user, str(average)))
+                    sc.rtm_send_message(channel, '<@{}> Average: {}'.format(user, str(average)))
 
                 # Assuming it doesn't have to be exactly 60 seconds
                 if time.time() - last_update > UPDATE_INTERVAL:
                     # Assuming general channel exists and public
-                    sc.rtm_send_message("general", "All users average: {}".format(str(calc_all_average(sums))))
+                    sc.rtm_send_message('general', 'All users average: {}'.format(str(calc_all_average(sums))))
                     last_update = time.time()
             # Sleep for 1 second
             time.sleep(READ_WEBSOCKET_DELAY)
--- tests/test_average_bot.py
+++ tests/test_average_bot.py
@@ -1,16 +1,63 @@
-#TODO implement
+from average_bot import *
+import random
+import string
+import copy
 import pytest
 
-def test_is_float_true():
-    assert True
+USERS_NUM = 6
+STRING_LEN = 6
+
+@pytest.fixture
+def sums():
+    sums1 = {}
+    for i in xrange(0, USERS_NUM):
+        rand_user = ''.join(random.choice(string.ascii_letters) for _ in range(STRING_LEN))
+        sums1[rand_user] = {}
+        sums1[rand_user]['sum'] = random.uniform(-1.0, 100.1)
+        sums1[rand_user]['count'] = random.uniform(0, 9)
+    return sums1
+
+def test_is_float():
+    assert is_float(random.uniform(-1.0, 2.9)) and \
+           is_float(str(random.uniform(-1.0, 2.9)))
 
 def test_is_float_false():
+    assert not is_float(''.join(random.choice(string.ascii_letters) for _ in range(STRING_LEN)))
+
+def test_calc_all_average(sums):
+    all_sum = sum([x.get('sum') for x in sums.values()])
+    counter = sum([x.get('count') for x in sums.values()])
+    if counter == 0:
+        assert 0 == calc_all_average(sums)
+    assert (all_sum / counter) ==  calc_all_average(sums)
+
+def test_calc_all_average_key_error(sums):
+    with pytest.raises(TypeError):
+        temp_sum = copy.deepcopy(sums)
+        new_user = ''.join(random.choice(string.ascii_letters) for _ in range(STRING_LEN))
+        while sums.has_key(new_user):
+            new_user = ''.join(random.choice(string.ascii_letters) for _ in range(STRING_LEN))
+        temp_sum[new_user] = {}
+        calc_all_average(temp_sum)
+
+def test_update_sums(sums):
+    new_user = ''.join(random.choice(string.ascii_letters) for _ in range(STRING_LEN))
+    rand_num = random.uniform(-1.0, 100.1)
+    is_new = True
+    if sums.has_key(new_user):
+        is_new = False
+    update_sums(sums, new_user, rand_num)
+    if is_new:
+        assert sums.get(new_user).get('sum') == rand_num
+    update_sums(sums, new_user, random.uniform(-1.0, 100.1))
     assert True
 
-def test_calc_all_average():
-    assert True
-
-def test_update_sums():
+def test_update_sums_new_user(sums):
+    new_user = ''.join(random.choice(string.ascii_letters) for _ in range(STRING_LEN))
+    rand_num = random.uniform(-1.0, 100.1)
+    while sums.has_key(new_user):
+        new_user = ''.join(random.choice(string.ascii_letters) for _ in range(STRING_LEN))
+    update_sums(sums, new_user, rand_num)
     assert True
 
 
